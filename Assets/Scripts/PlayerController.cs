6cb5764dccef7dbdbcefbc3ad5ddc089919db7c9
Amanda <amanda.lrib@gmail.com>
2023-02-07 22:08:46 -0300

    ✨Feature: Implement player rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] private float speed;
+    [SerializeField] private float rotation;
 
     private Rigidbody2D rigidbody;
 
@@ -26,8 +27,22 @@ public class PlayerController : MonoBehaviour
         float verticalMovement = Input.GetAxis("Vertical");
 
         Vector2 direction = new Vector2(horizontalMovement, verticalMovement).normalized;
-
+        
+        PlayerRotation();
+        
         rigidbody.velocity = new Vector2(direction.x * speed * Time.deltaTime, direction.y * speed * Time.deltaTime);
-
     }
+    private void PlayerRotation()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.UpArrow))
+            transform.Rotate(Vector3.forward * rotation * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.UpArrow))
+            transform.Rotate(-Vector3.forward * rotation * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.DownArrow))
+            transform.Rotate(-Vector3.forward * rotation * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.DownArrow))
+            transform.Rotate(Vector3.forward * rotation * Time.deltaTime);
+    }
+
+
 }
