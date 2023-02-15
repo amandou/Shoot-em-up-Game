e48b3ffebc8adf5a1f2da4dc3ede87afa20e6a04
Amanda <amanda.lrib@gmail.com>
2023-02-14 23:01:51 -0300

    ✨Feature: Damage enemy by bulletSO damage

--- Assets/Scripts/Player/PlayerBullet.cs
+++ Assets/Scripts/Player/PlayerBullet.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 
 public class PlayerBullet : MonoBehaviour
 {
+    public BulletSO bullet;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Enemy"))
         {
             Destroy(gameObject);
         }
+    }
+
+    public int GetBulletDamage()
+    {
+        return bullet.Damage;
     }
 }
