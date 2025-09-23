8ed42b7be9ed5eedccbe8d8295febfd0fdbc57ea
Amanda <amanda.lrib@gmail.com>
2025-09-23 14:27:08 -0300

    update: modify script name

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,9 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class ScoreSystem : MonoBehaviour
+public class ScoreManager : MonoBehaviour
 {
     [SerializeField] private int score;
     private TextMeshProUGUI scoreText;
