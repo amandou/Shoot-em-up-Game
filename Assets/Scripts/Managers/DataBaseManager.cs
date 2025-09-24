f2ce4202109e1a071db8e7e00d60d7c649bb518b
Amanda <amanda.lrib@gmail.com>
2025-09-23 21:29:46 -0300

    feature: Add name and score to firebase database

---
+++ Assets/Scripts/Managers/DataBaseManager.cs
@@ -0,0 +1,25 @@
+﻿using Firebase.Database;
+using UnityEngine;
+
+namespace Managers
+{
+    public class DataBaseManager : MonoBehaviour
+    {
+        private string _userId;
+        
+        private DatabaseReference _reference;
+        void Start() 
+        {
+            _userId = SystemInfo.deviceUniqueIdentifier;    
+            _reference = FirebaseDatabase.DefaultInstance.RootReference;
+            
+            AddNewUser(_userId,"Teste00", 1001);
+        }
+        
+        private void AddNewUser(string userId, string name, int score) {
+            var user = new User(name, score);
+            var json = JsonUtility.ToJson(user);
+            _reference.Child("users").Child(userId).SetRawJsonValueAsync(json);
+        }
+    }
+}
---
+++ Assets/Scripts/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Managers
+{
+    public class User
+    {
+        public string name;
+        public int score;
+
+        public User(string name, int score)
+        {
+            this.name = name;
+            this.score = score;
+        }
+
+    }
+}
